fd8378f1902a81e2c48751967507aa3c669b2f7c
jinfo-nhannguyen <nhan.nguyen@information.net>
2022-09-15 03:14:18 +0700

    add MONGO_URI

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var gracefulShutdown;
 var dbURI = 'mongodb://localhost/Bet';
 if (process.env.NODE_ENV === 'production') {
-    dbURI = process.env.MONGOLAB_URI;
+    dbURI = process.env.MONGO_URI;
 }
 
 mongoose.connect(dbURI);
